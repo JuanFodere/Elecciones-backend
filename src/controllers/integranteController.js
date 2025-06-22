8c73ae7adce97eed2961a773b29a0ea38b39e5e0
Juan Fodere <titanjuan14@gmail.com>
2025-06-22 20:10:39 -0300

    mas cambios jeje

--- src/controllers/integranteController.js
+++ src/controllers/integranteController.js
@@ -19,7 +19,28 @@ const confirmarAutoridad = async (req, res) => {
     res.status(500).json({ message: 'Error interno del servidor' });
     error: error.message
   }
-}
+};
+
+const verCircuitoAsignado = async (req, res) => {
+  const { ci, fecha } = req.body;
+  console.log(ci, fecha);
+
+  try {
+    const [rows] = await db.query('SELECT ID_circuito FROM integrante_mesa WHERE CI = ? and fecha = ?', [ci, fecha]);
+    const circuito = rows[0];
+
+    if (!circuito) {
+      return res.status(404).json({ message: 'Circuito no encontrado' });
+    }
+
+    res.json(circuito);
+  } catch (error) {
+    console.error('Error al obtener el circuito:', error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+    error: error.message
+  }
+};
 
 
-module.exports = { confirmarAutoridad };
+
+module.exports = { confirmarAutoridad, verCircuitoAsignado };
--- src/routes/integranteRoutes.js
+++ src/routes/integranteRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const {confirmarAutoridad} = require('../controllers/integranteController');
+const {verCircuitoAsignado} = require('../controllers/integranteController');
 
 router.post('/confirmarAutoridad', confirmarAutoridad);
+router.post('/circuitoAsignado', verCircuitoAsignado);
 
 module.exports = router;
