a4b48514f4c78294a3729f5ff4789ab305d1d14e
Juan Fodere <titanjuan14@gmail.com>
2025-07-02 23:18:43 -0300

    funcion de habilitacion de resultados

--- src/controllers/circuitoController.js
+++ src/controllers/circuitoController.js
@@ -17,6 +17,7 @@ const abrirCircuito = async (req, res) => {
 
     const [rows2] = await db.query('SELECT * FROM fueron_abiertos WHERE circuito = ?', [id]);
     const circuito2 = rows2[0];
+    console.log(circuito2);
 
     if (circuito2) {
       return res.status(400).json({ message: 'El circuito ya fue abierto anteriormente', exito: false });
@@ -72,7 +73,41 @@ const verEstadoCircuito = async (req, res) => {
   }
 }
 
+const habilitaResultados = async (req, res) => {
+  console.log("Habilita resultados");
+  const { id } = req.params;
+
+  try {
+    const [rows] = await db.query('SELECT * FROM circuito WHERE ID = ?', [id]);
+    const circuito = rows[0];
+
+    if (!circuito) {
+      return res.status(404).json({ message: 'Circuito no encontrado' });
+    }
+
+    const [rows2] = await db.query('SELECT abierto FROM circuito WHERE ID = ?', [id]);
+    const circuito2 = rows2[0];
+
+    if (circuito2.abierto == true) {
+      return res.status(400).json({ message: 'El circuito está abierto, no se pueden habilitar los resultados' });
+    }
+    
+    const [rows3] = await db.query('SELECT * FROM fueron_abiertos WHERE circuito = ?', [id]);
+    const circuito3 = rows3[0];
+
+    if (!circuito3) {
+      return res.status(400).json({ message: 'El circuito aún no ha sido habilitado' });
+    }
+    
+    res.json({habilitado: true, message: 'Resultados habilitados'});
+  } catch (error) {
+    console.error('Error al obtener el estado del circuito:', error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+  }
+}
 
 
 
-module.exports = {abrirCircuito, cerrarCircuito, verEstadoCircuito};
+
+
+module.exports = {abrirCircuito, cerrarCircuito, verEstadoCircuito, habilitaResultados};
--- src/routes/circuitoRoutes.js
+++ src/routes/circuitoRoutes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const { verEstadoCircuito } = require('../controllers/circuitoController');
 const { abrirCircuito } = require('../controllers/circuitoController'); 
 const { cerrarCircuito } = require('../controllers/circuitoController');
+const { habilitaResultados } = require('../controllers/circuitoController');
 
 router.get('/:id/estado', verEstadoCircuito);
 router.post('/:id/abrir', abrirCircuito);
 router.post('/:id/cerrar', cerrarCircuito);
+router.get('/:id/habilitaResultados', habilitaResultados);
 
 module.exports = router;
