4202f7e9c0a6e86746746324797dd28cd07ca60e
Juan Fodere <titanjuan14@gmail.com>
2025-07-05 17:57:04 -0300

    resultados arreglados

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'ANUSH1234.',
+    password: 'admin',
     database: 'eleccionesdb',
     port: 3306,
 }).promise();
