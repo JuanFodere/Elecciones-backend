b8497d3d69fb914126233771cd25acbbbf401d6b
PauAdjemian <paulaadjemian2004@gmail.com>
2025-07-06 23:15:09 -0300

    push final

--- src/controllers/votoController.js
+++ src/controllers/votoController.js
@@ -21,5 +21,19 @@ const votarLista = async (req, res) => {
   }
 }
 
+const marcarVoto = async (req, res) => {
+    const { circuito, fecha_elección, tipo_elección, ci } = req.body;
+    console.log("los datos pa", circuito, fecha_elección, tipo_elección, ci)
 
-module.exports = {votarLista};
+    try {
+    await db.query('UPDATE votante_eleccion SET circuito_votado = ? WHERE CI_votante = ? AND fecha_elección = ? AND tipo_elección = ?',[circuito,ci, fecha_elección, tipo_elección]);
+    
+    res.json({ message: 'Voto registrado con éxito', exito: true });
+  } catch (error) {
+    console.error('Error al registrar:', error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+    error: error.message
+  }
+}
+
+module.exports = {votarLista, marcarVoto};
--- src/routes/votoRoutes.js
+++ src/routes/votoRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const {votarLista} = require('../controllers/votoController');
+const {marcarVoto} = require('../controllers/votoController');
 
 router.post('/votar', votarLista);
+router.post('/marcar', marcarVoto);
 
 
 module.exports = router;
